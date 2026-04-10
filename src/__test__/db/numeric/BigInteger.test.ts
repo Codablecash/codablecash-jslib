d62eea751b6d8a5f1f47b367bb117e5b3193e336
iizuka <iizuka@alinous.org>
2026-04-10 21:41:22 +0800

    binary 02

--- src/__test__/db/numeric/BigInteger.test.ts
+++ src/__test__/db/numeric/BigInteger.test.ts
@@ -28,9 +28,8 @@ describe('BigInteger test', () => {
     let av = ans.getValue();
     expect(av).toBe(122n);
   })
- /*
+
   it('binary 01', () => {
-    // let a : BigInteger = new BigInteger(0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798n);
     let a : BigInteger = new BigInteger(0x01020304n);
 
     let num = a.binarySize();
@@ -42,7 +41,7 @@ describe('BigInteger test', () => {
 
     expect(a.equals(b)).toBe(true);
   })
-
+ 
   it('binary 02', () => {
     let a : BigInteger = new BigInteger(0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798n);
 
@@ -55,5 +54,5 @@ describe('BigInteger test', () => {
 
     expect(a.equals(b)).toBe(true);
   })
-    */
+
 })
