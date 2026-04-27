f4e5881a94dac5828a15659b238218c191c2b9b9
iizuka <iizuka@alinous.org>
2026-04-27 16:25:06 +0800

    server connecton sync

--- src/__test__/p2p/instance/p2p_client_connection.test.ts
+++ src/__test__/p2p/instance/p2p_client_connection.test.ts
@@ -1,3 +1,4 @@
+import { IpClientConnection } from "../../../blockchain_p2p/ipconnect/IpClientConnection";
 import { IpV4ServerConnection } from "../../../blockchain_p2p/ipconnect/IpV4ServerConnection";
 
 
@@ -7,9 +8,19 @@ describe('Client Connection test', () => {
         ipv4Server.initAddress("127.0.0.1", 6543);
         await ipv4Server.listen();
 
-        
+        try{
+            const client = new IpClientConnection();
 
-        ipv4Server.close();
+            await client.connect("127.0.0.1", 6543);
+
+
+            client.close();
+        }
+        catch(exceptionVar){
+            console.log(exceptionVar);
+        }
+
+        await ipv4Server.close();
     });
 
 })
--- src/blockchain_p2p/ipconnect/IpV4ServerConnection.ts
+++ src/blockchain_p2p/ipconnect/IpV4ServerConnection.ts
@@ -15,7 +15,7 @@ export class IpV4ServerConnection implements IServerSocket {
             });
 
             socket.on('end', () => {
-                console.log('server closed.' + this.host);
+                //console.log('server closed.' + this.host);
             });
 
             socket.on('error', (err: Error) => {
@@ -32,7 +32,7 @@ export class IpV4ServerConnection implements IServerSocket {
         this.port = port;
     }
     
-    public listen() : Promise<void> {
+    public async listen() : Promise<void> {
         return new Promise<void>((resolve) => {
             this.server.listen(this.port, this.host, () => {
                 console.log("TCP Server: Server listening on " + this.host + ":" + this.port);
@@ -41,7 +41,17 @@ export class IpV4ServerConnection implements IServerSocket {
         });
     }
 
-    public close() : void {
-        this.server.close();
+    public async close() : Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this.server.close((err?: Error) => {
+                if (err) {
+                    reject(err);
+                }
+                else{
+                     resolve();
+                }
+            });
+        });
+        
     }
 }
