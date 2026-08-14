20752b5f6c29869814dad60555d34ceeb43c4bda
iizuka <iizuka@alinous.org>
2026-08-14 12:23:02 +0800

    MMapSegment members

--- src/db/random_access_file/MMapSegment.ts
+++ src/db/random_access_file/MMapSegment.ts
@@ -1,5 +1,20 @@
+import { MMapSegments } from "./MMapSegments";
 
 
 export class MMapSegment {
+    protected mappedSize : number;
+    protected position : number;
+    protected buffer : Uint8Array;
+    protected parent : MMapSegments;
+    protected dirty : boolean;
+
+    constructor(mappedSize : number, position : number, parent : MMapSegments){
+        this.mappedSize = mappedSize;
+        this.position = position;
+        this.buffer = new Uint8Array(this.mappedSize);
+        this.parent = parent;
+        this.dirty = false;
+    }
+
     
 }
