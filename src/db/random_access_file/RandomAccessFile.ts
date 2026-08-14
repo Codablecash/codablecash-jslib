d519a71d48fb3bd11a8eeb37f5521b2b5059e536
iizuka <iizuka@alinous.org>
2026-08-14 17:56:45 +0800

    open, close

--- src/db/random_access_file/RandomAccessFile.ts
+++ src/db/random_access_file/RandomAccessFile.ts
@@ -1,5 +1,7 @@
 import { CFile } from "../base_io/CFile";
 import { FileDescriptor } from "../osenv/FileDescriptor";
+import { FileIOException } from "../osenv/FileIOException";
+import { Os, SeekOrigin } from "../osenv/Os";
 import { DiskCacheManager } from "./DiskCacheManager";
 import { MMapSegments } from "./MMapSegments";
 
@@ -13,7 +15,9 @@ export class RandomAccessFile {
     private fd : FileDescriptor | null;
     private mmapSegments : MMapSegments | null;
 
-    constructor(file : CFile, diskCacheManager : DiskCacheManager, pageSize : number){
+    private static PAGE_NUM_CACHE = 4;
+
+    constructor(file : CFile, diskCacheManager : DiskCacheManager, pageSize : number = 4096){
         this.position = 0;
         this.fileSize = 0;
         this.diskCacheManager = diskCacheManager;
@@ -27,4 +31,77 @@ export class RandomAccessFile {
     public exists() : boolean {
         return this.file.exists();
     }
+
+    public open(sync : boolean) {
+        this.fd = Os.openFile2ReadWrite(this.file, sync);
+
+        if(!this.fd.isOpened()){
+            throw new FileIOException("Failed in opening file");
+        }
+
+        this.position = 0;
+        this.fileSize = this.file.length();
+
+        let segmentSize = this.getSegmentSize();
+	    this.mmapSegments = new MMapSegments(this.fileSize, segmentSize);
+
+        if(this.fileSize == 0){
+            this.setLength(this.pageSize * RandomAccessFile.PAGE_NUM_CACHE);
+        }
+    }
+
+    public close() {
+        if(this.fd != null && !this.fd.isOpened()) {
+            return;
+        }
+
+        if(this.mmapSegments != null && this.fd != null){
+            this.mmapSegments.clearElements(this.diskCacheManager, this.fd);
+            Os.closeFileDescriptor(this.fd);
+        }
+    }
+
+    public getSegmentSize() : number {
+        return this.pageSize * RandomAccessFile.PAGE_NUM_CACHE;
+    }
+
+    public async setLength(newLength : number) : Promise<void> {
+        if(!this.fd?.isOpened()){
+            throw new FileIOException("File is not opened.");
+        }
+        if(newLength <= this.fileSize){
+            return;
+        }
+
+        let newSize = newLength - this.fileSize;
+        let numBlocks = newSize / this.pageSize;
+        let modBytes =  newSize % this.pageSize;
+
+        let fpos = Os.seekFile(this.fd, 0, SeekOrigin.FROM_END);
+
+        let n = 0;
+        let tmp = new Uint8Array(this.pageSize);
+        tmp.fill(0, 0, this.pageSize);
+
+        for(let i = 0; i != numBlocks; ++i){
+            n = await Os.write2File(this.fd, tmp, this.pageSize);
+
+            if(n != this.pageSize){
+                throw new FileIOException("filed in writing file.");
+            }
+        }
+
+        n = await Os.write2File(this.fd, tmp, modBytes);
+        if(n != modBytes){
+            throw new FileIOException("filed in writing file.");
+        }
+
+        let ret = Os.syncFile(this.fd);
+
+        fpos = Os.seekFile(this.fd, 0, SeekOrigin.CURRENT_POS); // get the position of the last
+
+        this.fileSize = fpos;
+        await this.mmapSegments?.onResized(this.fileSize, this.fd, this.diskCacheManager);
+    }
+
 }
