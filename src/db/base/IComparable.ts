538b2b8fcc5f99ee7ae1b96281d9362cb31e2862
iizuka <iizuka@alinous.org>
2026-08-14 11:56:54 +0800

    comapre

---
+++ src/db/base/IComparable.ts
@@ -0,0 +1,7 @@
+
+
+export class IComparable {
+    compareTo(other : IComparable | null) : number {
+        return 0;
+    }
+}
--- src/db/base/RawLinkedList.ts
+++ src/db/base/RawLinkedList.ts
@@ -1,23 +1,39 @@
+import { IComparable } from "./IComparable";
 
 
-export class RawLinkedList<T> {
+type RawLinkedListCmp<T extends IComparable> = (x : RawLinkedListElement<T>, y : RawLinkedListElement<T>) => number;
+
+function defaultCompare<T extends IComparable>(x : RawLinkedListElement<T>, y : RawLinkedListElement<T>) : number {
+
+    return 0;
+}
+
+
+export class RawLinkedList<T extends IComparable> {
     protected root : RawLinkedListElement<T> | null;
     protected last : RawLinkedListElement<T> | null;
     protected length : number;
+    private compareFunctor : RawLinkedListCmp<T>;
 
-    constructor(){
+    constructor(cmp : RawLinkedListCmp<T> = defaultCompare<T>){
         this.root = null;
         this.last = null;
         this.length = 0;
+        this.compareFunctor = cmp;
     }
 
     public getRoot() : RawLinkedListElement<T> | null {
         return this.root;
     }
 
+    public iterator() : RawLinkedListIterator<T> {
+        let it = new RawLinkedListIterator<T>(this);
+        return it;
+    } 
+
 }
 
-export class RawLinkedListElement<T> {
+export class RawLinkedListElement<T extends IComparable> {
     public data : T | null;
     public next : RawLinkedListElement<T> | null;
     public prev : RawLinkedListElement<T> | null;
@@ -27,9 +43,24 @@ export class RawLinkedListElement<T> {
         this.next = null;
         this.prev = null;
     }
+
+    public compareTo(other : RawLinkedListElement<T>) : number {
+        if(this.data == null){
+            if(other.data == null){
+                return 0;
+            }
+            else {
+                return 1;
+            }
+        }
+
+        let d = other != null ? other.data : null;
+
+        return this.data.compareTo(d);
+    }
 }
 
-export class Iterator<T> {
+export class RawLinkedListIterator<T extends IComparable> {
     private list : RawLinkedList<T>;
     private current : RawLinkedListElement<T> | null;
 
