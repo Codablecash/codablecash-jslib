b9e30c515f5f768c839cf2530a061928987d36f4
iizuka <iizuka@alinous.org>
2026-08-15 23:11:58 +0800

    checkTestData

--- src/__test__/db/db_filestore/variable_block_filestore.test.ts
+++ src/__test__/db/db_filestore/variable_block_filestore.test.ts
@@ -12,6 +12,18 @@ function makeTestData(start : number, length : number) {
 	return ptr;
 }
 
+function checkTestData(start : number, data : Uint8Array, length: number) {
+	for(let i = 0; i != length; ++i){
+		let ch = start % 128;
+		start++;
+
+		if(data[i] != ch){
+			return false;
+		}
+	}
+	return true;
+}
+
 describe('TestVariableBlockFileStoreGroup', () => {
     it('addSimpleRange', () => {
 
