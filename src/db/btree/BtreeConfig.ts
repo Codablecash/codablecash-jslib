0c7198525a07d573998860baa2bf7c68649cd27a
iizuka <iizuka@alinous.org>
2026-08-17 11:19:33 +0800

    BtreeConfig

--- src/db/btree/BtreeConfig.ts
+++ src/db/btree/BtreeConfig.ts
@@ -1,5 +1,7 @@
+import { ByteBuffer } from "../base_io/ByteBuffer";
+import { IBlockObject } from "../filestore_block/IBlockObject";
 
-export class BtreeConfig {
+export class BtreeConfig implements IBlockObject {
     private defaultSize : number;
     private blockSize : number;
     private nodeNumber : number;
@@ -10,5 +12,24 @@ export class BtreeConfig {
         this.nodeNumber = 8;
     }
 
-    
+    public binarySize(): number {
+        return 8 + 8 + 8;
+    }
+    public toBinary(out: ByteBuffer): void {
+        out.putLong(this.defaultSize);
+        out.putLong(this.blockSize);
+        out.putLong(this.nodeNumber);
+    }
+
+    public copyData(): IBlockObject {
+        let inst =  new BtreeConfig();
+
+        inst.defaultSize = this.defaultSize;
+        inst.blockSize = this.blockSize;
+        inst.nodeNumber = this.nodeNumber;
+
+        return inst;
+    }
+
+
 }
