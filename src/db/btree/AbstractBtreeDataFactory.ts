ed15cea5abe0adbc5c68d86dc09e655cab62437e
iizuka <iizuka@alinous.org>
2026-08-17 12:33:32 +0800

    AbstractBtreeDataFactory

--- src/db/btree/AbstractBtreeDataFactory.ts
+++ src/db/btree/AbstractBtreeDataFactory.ts
@@ -1,5 +1,12 @@
+import { ByteBuffer } from "../base_io/ByteBuffer";
+import { IBlockObject } from "../filestore_block/IBlockObject";
+import { AbstractBtreeKey } from "./AbstractBtreeKey";
+import { BtreeStorage } from "./BtreeStorage";
+import { DataNode } from "./DataNode";
 
 
-export class AbstractBtreeDataFactory {
-    
+export abstract class AbstractBtreeDataFactory {
+    public abstract makeDataFromBinary(input : ByteBuffer) : IBlockObject;
+    public abstract registerData(key : AbstractBtreeKey, data: IBlockObject, dataNode : DataNode, store: BtreeStorage) : void;
+    public abstract beforeRemove(dataNode : DataNode, store : BtreeStorage, key: AbstractBtreeKey) : boolean;
 }
--- src/db/btreekey/KeyFormatException.ts
+++ src/db/btreekey/KeyFormatException.ts
@@ -1,5 +1,5 @@
 import { Exception } from "../base/Exception";
 
 export class KeyFormatException extends Exception {
-
+    
 }
