65d7f90b412cdee37b8bce92d4be3d96c230f193
iizuka <iizuka@alinous.org>
2026-08-17 11:37:43 +0800

    keys

---
+++ src/db/btreekey/InfinityKey.ts
@@ -0,0 +1,27 @@
+import { ByteBuffer } from "../base_io/ByteBuffer";
+import { AbstractBtreeKey } from "../btree/AbstractBtreeKey";
+
+
+export class InfinityKey extends AbstractBtreeKey {
+
+    
+    public binarySize(): number {
+        throw new Error("Method not implemented.");
+    }
+    public toBinary(out: ByteBuffer): void {
+        throw new Error("Method not implemented.");
+    }
+    public isInfinity(): boolean {
+        throw new Error("Method not implemented.");
+    }
+    public isNull(): boolean {
+        throw new Error("Method not implemented.");
+    }
+    public compareTo(): number {
+        throw new Error("Method not implemented.");
+    }
+    public clone(): AbstractBtreeKey {
+        throw new Error("Method not implemented.");
+    }
+
+}
---
+++ src/db/btreekey/NullKey.ts
@@ -0,0 +1,26 @@
+import { ByteBuffer } from "../base_io/ByteBuffer";
+import { AbstractBtreeKey } from "../btree/AbstractBtreeKey";
+
+export class NullKey extends AbstractBtreeKey {
+
+    
+    public binarySize(): number {
+        throw new Error("Method not implemented.");
+    }
+    public toBinary(out: ByteBuffer): void {
+        throw new Error("Method not implemented.");
+    }
+    public isInfinity(): boolean {
+        throw new Error("Method not implemented.");
+    }
+    public isNull(): boolean {
+        throw new Error("Method not implemented.");
+    }
+    public compareTo(): number {
+        throw new Error("Method not implemented.");
+    }
+    public clone(): AbstractBtreeKey {
+        throw new Error("Method not implemented.");
+    }
+
+}
