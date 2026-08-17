81db6e4554973c4ce9cecdaf31fb7f1bb53aca39
iizuka <iizuka@alinous.org>
2026-08-17 09:45:29 +0800

    changed test config

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  testTimeout: 120000,
+  workerThreads: false,
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
