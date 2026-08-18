63d09d483c4c0d6688ebdca8391b3db6f97e027e
iizuka <iizuka@alinous.org>
2026-08-18 23:34:10 +0800

    btreetestbench

---
+++ src/__test__/db/btree/test_btree_base.test.ts
@@ -0,0 +1,16 @@
+import { InfinityKey } from "../../../db/btreekey/InfinityKey"
+import { ULongKey } from "../../../db/btreekey/ULongKey";
+
+describe('TestBTreeGroup', () => {
+    it('infinityKey', () => {
+        let key = new InfinityKey();
+        let key2 = <InfinityKey>key.clone();
+        let ulkey = new ULongKey(100);
+
+        expect(key.compareTo(key2)).toBe(0);
+        expect(key.compareTo(ulkey) > 0).toBe(true);
+
+        expect(!key.isNull()).toBe(true);
+        expect(!ulkey.isNull()).toBe(true);
+    })
+})
