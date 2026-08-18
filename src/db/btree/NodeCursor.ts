926c9a988dccac74138e769ee9db462bd2fe2d0f
iizuka <iizuka@alinous.org>
2026-08-18 19:53:17 +0800

    insert

--- src/db/btree/NodeCursor.ts
+++ src/db/btree/NodeCursor.ts
@@ -52,7 +52,7 @@ export class NodeCursor {
     }
 
     public insert(key : AbstractBtreeKey, data : IBlockObject) : void {
-        /* let current = this.top();
+        let current = this.top();
         //__ASSERT_TREE
 
         // check data nodes
@@ -60,8 +60,8 @@ export class NodeCursor {
 
         // down to leaf node
         while(!current.isLeaf()){
-            uint64_t nextFpos = current.getNextChild(key);
-            NodeHandle* nh = this.store.loadNode(nextFpos);
+            let nextFpos = current.getNextChild(key);
+            let nh = this.store.loadNode(nextFpos);
 
             current = new NodePosition(nh);
             this.push(current);
@@ -70,11 +70,11 @@ export class NodeCursor {
         }
 
         // 1. already has key
-        NodeHandle* sameKeyDataNode = current.hasKey(key);
-        if(sameKeyDataNode != nullptr){
-            DataNode* dnode = sameKeyDataNode.toDataNode();
+        let sameKeyDataNode = current.hasKey(key);
+        if(sameKeyDataNode != null){
+            let dnode = sameKeyDataNode.toDataNode();
 
-            const AbstractBtreeDataFactory* dfactory = this.store.getDataFactory();
+            let dfactory = this.store.getDataFactory();
             dfactory.registerData(key, data, dnode, this.store);
 
             this.store.updateNode(dnode);
@@ -84,23 +84,23 @@ export class NodeCursor {
 
         // 2. Add key, then check whether the node is full or not
         if(current.isFull(this.nodeNumber)){
-            splitLeafNode(key, data);
+            this.splitLeafNode(key, data);
 
             return;
         }
 
         // simply add data
-        DataNode dataNode(key.clone());
+        let dataNode = new DataNode(key.clone());
 
-        const AbstractBtreeDataFactory* dfactory = this.store.getDataFactory();
-        dfactory.registerData(key, data, &dataNode, this.store);
+        let dfactory = this.store.getDataFactory();
+        dfactory.registerData(key, data, dataNode, this.store);
 
-        uint64_t newDataNodeFpos = this.store.storeNode(&dataNode);
+        let newDataNodeFpos = this.store.storeNode(dataNode);
 
         current.addNode(key, newDataNodeFpos, this.nodeNumber);
         current.save(this.store);
 
-        current.loadInnerNodes(this.store);*/
+        current.loadInnerNodes(this.store);
     }
 
     public splitLeafNode(key : AbstractBtreeKey, data : IBlockObject) : void {
