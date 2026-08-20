8f6e6d1db14f309450d0537bc3a31cdedb2268c6
iizuka <tomohiro.iizuka@codablecash.org>
2026-08-20 17:21:33 +0800

    toAddressDescriptor()

---
+++ src/blockchain/bc_base/AbstractAddress.ts
@@ -0,0 +1,44 @@
+import { ByteBuffer } from "../../db/base_io/ByteBuffer";
+import { IBlockObject } from "../../db/filestore_block/IBlockObject";
+import { AddressDescriptor } from "./AddressDescriptor";
+
+export abstract class AbstractAddress implements IBlockObject{
+    private zone : number;
+
+    constructor(zone : number) {
+        this.zone = zone;
+    }
+
+    public abstract getType() : number;
+
+    public abstract binarySize(): number;
+    public abstract toBinary(out: ByteBuffer): void;
+    public abstract fromBinary(input : ByteBuffer) : void;
+
+    public abstract copyData(): IBlockObject;
+
+	public abstract getPrefix() : string;
+	public abstract getBodyPart() : ByteBuffer;
+
+    public toAddressDescriptor() : AddressDescriptor {
+        let prefixstr = this.getPrefix();
+        let prefix = Buffer.from(prefixstr, "utf8");
+
+        /*
+        char zonech[4];
+        Mem::memset(zonech, 0, 4);
+        ::sprintf(zonech, "%03d", (uint8_t)this->zone);
+        */
+        let zonestr = this.zone.toString(10).padStart(3);
+        let zonech = Buffer.from(zonestr, "utf8");
+
+        let body = this.getBodyPart();
+
+        body.position(0);
+        let charstr = body.toUint8Array();
+    
+        let length = body.limit();
+
+        return new AddressDescriptor(prefix, zonech, charstr, length);
+    }
+}
---
+++ src/blockchain/bc_base/AddressDescriptor.ts
@@ -0,0 +1,7 @@
+
+export class AddressDescriptor {
+
+    constructor(prefix : Uint8Array, zone : Uint8Array, body : Uint8Array, bodylength : number){
+
+    }
+}
