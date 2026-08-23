ae604105a15dbf5b439eb44e7b4dcf9a5d17b1f9
iizuka <iizuka@alinous.org>
2026-08-23 12:17:48 +0800

    AddressDescriptorData

--- src/blockchain/bc_base_utxo_index/AddressDescriptorData.ts
+++ src/blockchain/bc_base_utxo_index/AddressDescriptorData.ts
@@ -1,9 +1,29 @@
+import { ByteBuffer } from "../../db/base_io/ByteBuffer";
+import { IBlockObject } from "../../db/filestore_block/IBlockObject";
 import { AddressDescriptor } from "../bc_base/AddressDescriptor";
 
-export class AddressDescriptorData {
+export class AddressDescriptorData implements IBlockObject {
     private desc : AddressDescriptor;
 
     constructor(desc : AddressDescriptor){
         this.desc = <AddressDescriptor>desc.copyData();
+    }
+
+    public binarySize(): number {
+        let total = this.desc.binarySize();
+
+        return total;
+    }
+    public toBinary(out: ByteBuffer): void {
+        this.desc.toBinary(out);
+    }
+    public fromBinary(input : ByteBuffer) {
+        let desc = AddressDescriptor.createFromBinary(input);
+        return new AddressDescriptorData(desc);
+    }
+
+    public copyData(): IBlockObject {
+        let inst = new AddressDescriptorData(this.desc);
+        return inst;
     }
 }
