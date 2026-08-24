c4def30d9c326590b0c3231902a70073c3e78c3d
iizuka <iizuka@alinous.org>
2026-08-24 17:05:39 +0800

    AddressDescriptorKey

---
+++ src/blockchain/bc_base_utxo_index/AddressDescriptorKey.ts
@@ -0,0 +1,62 @@
+import { ByteBuffer } from "../../db/base_io/ByteBuffer";
+import { AbstractBtreeKey } from "../../db/btree/AbstractBtreeKey";
+import { AddressDescriptor } from "../bc_base/AddressDescriptor";
+import { AbstractUtxo } from "../bc_trx/AbstractUtxo";
+import { AddressDescriptorKeyFactory } from "./AddressDescriptorKeyFactory";
+
+
+export class AddressDescriptorKey extends AbstractBtreeKey {
+	private desc : AddressDescriptor;
+	private utxo : AbstractUtxo | null; // remove param
+
+    constructor(desc : AddressDescriptor) {
+        super();
+        this.desc = <AddressDescriptor>(desc.copyData());
+        this.utxo = null;
+    }
+
+	public isInfinity() : boolean {
+        return false;
+    }
+	public isNull() : boolean {
+        return false;
+    }
+
+    public binarySize() : number {
+        let size = 4; //sizeof(uint32_t);
+        size += this.desc.binarySize();
+
+        return size;
+    }
+
+    public toBinary(out : ByteBuffer) : void {
+        out.putInt(AddressDescriptorKeyFactory.ADDRESS_DESC_KEY);
+
+        this.desc.toBinary(out);
+    }
+
+    public static fromBinary(input : ByteBuffer) : AddressDescriptorKey {
+        let desc = AddressDescriptor.createFromBinary(input);
+
+        return new AddressDescriptorKey(desc);
+    }
+
+    public compareTo(key: AbstractBtreeKey): number {
+        if(key.isInfinity()){
+            return -1;
+        }
+        else if(key.isNull()){
+            return 1;
+        }
+
+        let other = <AddressDescriptorKey>(key);
+
+        return this.desc.compareTo(other.desc);
+    }
+    public clone(): AbstractBtreeKey {
+        let inst = new AddressDescriptorKey(this.desc);
+        inst.utxo = this.utxo != null ? <AbstractUtxo>this.utxo.copyData() : null;
+
+        return inst;
+    }
+}
--- src/blockchain/bc_base_utxo_index/AddressDescriptorKeyFactory.ts
+++ src/blockchain/bc_base_utxo_index/AddressDescriptorKeyFactory.ts
@@ -1,4 +1,24 @@
+import { ByteBuffer } from "../../db/base_io/ByteBuffer";
+import { AbstractBtreeKey } from "../../db/btree/AbstractBtreeKey";
+import { BtreeKeyFactory } from "../../db/btreekey/BtreeKeyFactory";
+import { AddressDescriptorKey } from "./AddressDescriptorKey";
 
-export class AddressDescriptorKeyFactory {
-    
+export class AddressDescriptorKeyFactory extends BtreeKeyFactory {
+    public static readonly ADDRESS_DESC_KEY = 0x14;
+
+    constructor() {
+        super();
+    }
+
+	public fromBinary(keyType : number, input : ByteBuffer) : AbstractBtreeKey {
+        if(keyType == AddressDescriptorKeyFactory.ADDRESS_DESC_KEY){
+            return AddressDescriptorKey.fromBinary(input);
+        }
+
+        return super.fromBinary(keyType, input);
+    }
+
+	public copy() : BtreeKeyFactory {
+        return new AddressDescriptorKeyFactory();
+    }
 }
