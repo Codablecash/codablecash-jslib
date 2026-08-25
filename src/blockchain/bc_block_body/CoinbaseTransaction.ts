492787676cdfdd88ad0b772f902a5bd8629eae70
iizuka <iizuka@alinous.org>
2026-08-25 21:27:57 +0800

    CoinbaseTransaction

--- src/blockchain/bc_block_body/CoinbaseTransaction.ts
+++ src/blockchain/bc_block_body/CoinbaseTransaction.ts
@@ -1,48 +1,83 @@
+import { Sha256 } from "../../base/crypto/Sha256";
+import { NullPointerException } from "../../db/base/NullPointerException";
 import { ByteBuffer } from "../../db/base_io/ByteBuffer";
 import { IBlockObject } from "../../db/filestore_block/IBlockObject";
 import { BalanceUnit } from "../bc_base/BalanceUnit";
 import { AbstractBlockchainTransaction } from "../bc_trx/AbstractBlockchainTransaction";
 import { AbstractUtxo } from "../bc_trx/AbstractUtxo";
 import { AbstractUtxoReference } from "../bc_trx/AbstractUtxoReference";
+import { TransactionId } from "../bc_trx/TransactionId";
 import { AbstractBlockRewordTransaction } from "./AbstractBlockRewordTransaction";
+import { Coinbase } from "./Coinbase";
 
 
 export class CoinbaseTransaction extends AbstractBlockRewordTransaction {
+    private coinbase :Coinbase | null;
+
+    constructor(){
+        super();
+        this.coinbase = null;
+    }
 
     public getType(): number {
         return AbstractBlockchainTransaction.TRX_TYPE_COIN_BASE;
     }
+
     public build(): void {
-        throw new Error("Method not implemented.");
+        this.setUtxoNonce();
+
+        let capacity = this.binarySize();
+        let buff = ByteBuffer.allocateWithEndian(capacity, true);
+
+        this.toBinary(buff);
+        buff.position(0);
+
+        let sha = Sha256.sha256(buff.toUint8Array(), true);
+
+        this.trxId = new TransactionId(sha.toUint8Array(), sha.limit());
     }
+
     public getFee(): BalanceUnit {
-        throw new Error("Method not implemented.");
+        return new BalanceUnit(0);
     }
     public getFeeRate(): BalanceUnit {
-        throw new Error("Method not implemented.");
+        return new BalanceUnit(0);
     }
+
     public getUtxoSize(): number {
-        throw new Error("Method not implemented.");
+        return super.getUtxoSize();
     }
     public getUtxo(i: number): AbstractUtxo {
-        throw new Error("Method not implemented.");
+        return super.getUtxo(i);
     }
     public getUtxoReferenceSize(): number {
-        throw new Error("Method not implemented.");
+        return super.getUtxoReferenceSize();
     }
     public getUtxoReference(i: number): AbstractUtxoReference {
-        throw new Error("Method not implemented.");
+        return super.getUtxoReference(i);
     }
+
     public binarySize(): number {
-        throw new Error("Method not implemented.");
+        if(this.coinbase != null){
+            let total = this.__binarySize();
+            total += this.coinbase.binarySize();
+
+            return total;
+        }
+        throw new NullPointerException("CoinbaseTransaction.binarySize()");
     }
     public toBinary(out: ByteBuffer): void {
-        throw new Error("Method not implemented.");
+        if(this.coinbase != null){
+            this.__toBinary(out);
+            this.coinbase.toBinary(out);
+            return;
+        }
+        throw new NullPointerException("CoinbaseTransaction.toBinary()");
     }
     public fromBinary(input: ByteBuffer): void {
-        throw new Error("Method not implemented.");
-    }
-    public copyData(): IBlockObject {
-        throw new Error("Method not implemented.");
+        this.__fromBinary(input);
+
+        let ref = AbstractUtxoReference.createFromBinary(input);
+        this.coinbase = <Coinbase>(ref);
     }
 }
