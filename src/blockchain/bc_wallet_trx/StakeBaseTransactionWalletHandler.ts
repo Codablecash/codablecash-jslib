2fc5871f4c048bc6fb953b21f3a2dad45da8a2f3
iizuka <iizuka@alinous.org>
2026-08-27 20:56:01 +0800

    RevokeMissedTicketWalletHandler

---
+++ src/blockchain/bc_wallet_trx/StakeBaseTransactionWalletHandler.ts
@@ -0,0 +1,14 @@
+import { AbstractBlockchainTransaction } from "../bc_trx/AbstractBlockchainTransaction";
+import { WalletAccount } from "../bc_wallet/WalletAccount";
+import { AbstractWalletTransactionHandler } from "./AbstractWalletTransactionHandler";
+
+export class StakeBaseTransactionWalletHandler extends AbstractWalletTransactionHandler {
+    constructor(account : WalletAccount){
+        super(account);
+    }
+
+    public importTransaction(__trx : AbstractBlockchainTransaction) : void {
+        this.__importTransaction(__trx);
+    }
+    
+}
