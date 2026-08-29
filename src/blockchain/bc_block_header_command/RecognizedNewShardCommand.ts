b1c4e455930ffc002f62c1d100303c44c90da8e0
iizuka <iizuka@alinous.org>
2026-08-29 13:09:54 +0800

    NotifyZoneExtendRequestedTransaction base

--- src/blockchain/bc_block_header_command/RecognizedNewShardCommand.ts
+++ src/blockchain/bc_block_header_command/RecognizedNewShardCommand.ts
@@ -1,12 +1,33 @@
+import { ByteBuffer } from "../../db/base_io/ByteBuffer";
+import { IBlockObject } from "../../db/filestore_block/IBlockObject";
+import { NotifyZoneExtendRequestedTransaction } from "../bc_status_cache_extend_shard/NotifyZoneExtendRequestedTransaction";
 import { MerkleCertificate } from "../merkletree/MerkleCertificate";
 import { AbstractBlockHeaderCommand } from "./AbstractBlockHeaderCommand";
 
 export class RecognizedNewShardCommand extends AbstractBlockHeaderCommand {
-	private trx : NotifyZoneExtendRequestedTransaction;
-	private certificate : MerkleCertificate;
+
+	private trx : NotifyZoneExtendRequestedTransaction | null;
+	private certificate : MerkleCertificate | null;
 
     constructor(){
         super(AbstractBlockHeaderCommand.RECOGNIZED_SHARD_COMMAND);
+
+        this.trx = null;
+        this.certificate = null;
+    }
+
+    public binarySize(): number {
+        throw new Error("Method not implemented.");
+    }
+    public toBinary(out: ByteBuffer): void {
+        throw new Error("Method not implemented.");
+    }
+    public fromBinary(input: ByteBuffer): void {
+        throw new Error("Method not implemented.");
+    }
+
+    public copyData(): IBlockObject {
+        throw new Error("Method not implemented.");
     }
 
 
