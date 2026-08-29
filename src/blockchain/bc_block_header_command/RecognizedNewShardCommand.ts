5c553865fe445ab247c988d9c11d9bca41631157
iizuka <iizuka@alinous.org>
2026-08-29 13:51:12 +0800

    RecognizedNewShardCommand

--- src/blockchain/bc_block_header_command/RecognizedNewShardCommand.ts
+++ src/blockchain/bc_block_header_command/RecognizedNewShardCommand.ts
@@ -1,6 +1,8 @@
+import { NullPointerException } from "../../db/base/NullPointerException";
 import { ByteBuffer } from "../../db/base_io/ByteBuffer";
 import { IBlockObject } from "../../db/filestore_block/IBlockObject";
 import { NotifyZoneExtendRequestedTransaction } from "../bc_status_cache_extend_shard/NotifyZoneExtendRequestedTransaction";
+import { AbstractBlockchainTransaction } from "../bc_trx/AbstractBlockchainTransaction";
 import { MerkleCertificate } from "../merkletree/MerkleCertificate";
 import { AbstractBlockHeaderCommand } from "./AbstractBlockHeaderCommand";
 
@@ -17,19 +19,43 @@ export class RecognizedNewShardCommand extends AbstractBlockHeaderCommand {
     }
 
     public binarySize(): number {
-        throw new Error("Method not implemented.");
+        if(this.trx != null && this.certificate != null){
+            let total = 2; // sizeof(uint16_t);
+
+            total += this.trx.binarySize();
+            total += this.certificate.binarySize();
+
+            return total;
+        }
+        throw new NullPointerException("RecognizedNewShardCommand.binarySize()");
     }
     public toBinary(out: ByteBuffer): void {
-        throw new Error("Method not implemented.");
+        if(this.trx != null && this.certificate != null){
+            out.putShort(this.type);
+
+            this.trx.toBinary(out);
+            this.certificate.toBinary(out);
+        }
     }
     public fromBinary(input: ByteBuffer): void {
-        throw new Error("Method not implemented.");
+        let atrx = AbstractBlockchainTransaction.createFromBinary(input);
+        let ntrx = <NotifyZoneExtendRequestedTransaction>(atrx);
+
+        this.trx = ntrx;
+
+        this.certificate = MerkleCertificate.createFromBinary(input);
     }
 
     public copyData(): IBlockObject {
-        throw new Error("Method not implemented.");
+        if(this.trx != null && this.certificate != null){
+            let inst = new RecognizedNewShardCommand();
+            inst.trx = <NotifyZoneExtendRequestedTransaction>this.trx.copyData();
+            inst.certificate = <MerkleCertificate>this.certificate.copyData();
+
+            inst.buildCommandId();
+
+            return inst;
+        }
+        throw new NullPointerException("RecognizedNewShardCommand.copyData()");
     }
-
-
-    // TODO implements
 }
