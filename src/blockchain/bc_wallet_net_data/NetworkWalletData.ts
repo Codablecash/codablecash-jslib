d864cb82b0020296d214690ea555e4f17aa26a2a
iizuka <iizuka@alinous.org>
2026-08-29 16:08:19 +0800

    added commnet

--- src/blockchain/bc_wallet_net_data/NetworkWalletData.ts
+++ src/blockchain/bc_wallet_net_data/NetworkWalletData.ts
@@ -1,5 +1,32 @@
 
 export class NetworkWalletData {
     
+
+    /*
+	File* baseDir;
+	ISystemLogger* logger;
+	const CodablecashSystemParam *config;
+
+	// wallet (Finalized Data)
+	HdWallet* hdWallet;
+	uint64_t finalizedHeight;
+	uint16_t zone;
+
+	// unfinalized chain
+	TransactionGroupDataStore* transactionGroupData;
+
+	BlockHeaderStoreManager* headerManager;
+	HeadBlockDetector* detector;
+
+	// memory pool
+	WalletMemoryPool* mempool;
+
+	// store
+	StatusStore* statusStore;
+
+	// management account
+	ManagementAccountsCollection* managementAccounts;
+
+    */
     // TODO implement
 }
