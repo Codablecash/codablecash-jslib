4c1932db667fa39c5c0bfe26ca3a822753812a48
iizuka <iizuka@alinous.org>
2026-08-29 17:34:42 +0800

    ManagementAccountsCollection

--- src/blockchain/bc_wallet_net_data_management/ManagementAccountsCollection.ts
+++ src/blockchain/bc_wallet_net_data_management/ManagementAccountsCollection.ts
@@ -1,4 +1,7 @@
 import { ArrayList } from "../../db/base/ArrayList";
+import { NullPointerException } from "../../db/base/NullPointerException";
+import { TransactionId } from "../bc_trx/TransactionId";
+import { ManagedUtxoCacheRecord } from "./ManagedUtxoCacheRecord";
 import { ManagementAccount } from "./ManagementAccount";
 
 
@@ -13,5 +16,82 @@ export class ManagementAccountsCollection {
 
     private list : ArrayList<ManagementAccount>;
 
-    // TODO implement
+    constructor(){
+        this.list = new ArrayList<ManagementAccount>();
+        this.list.addElement(new ManagementAccount(ManagedUtxoCacheRecord.FINALIZED));
+        this.list.addElement(new ManagementAccount(ManagedUtxoCacheRecord.UNFINALIZED));
+        this.list.addElement(new ManagementAccount(ManagedUtxoCacheRecord.MEMPOOL));
+    }
+
+    public resetAll() : void {
+        this.resetFinalized();
+        this.resetUnfinalized();
+        this.resetMempool();
+    }
+
+    public resetFinalized() : void {
+        let account = this.list.get(ManagementAccountsCollection.POS_FINALIZED);
+        if(account != null){
+            account.reset();
+        }
+    }
+
+    public resetUnfinalized() : void {
+        let account = this.list.get(ManagementAccountsCollection.POS_UNFINALIZED);
+        if(account != null){
+            account.reset();
+        }
+    }
+
+    public resetMempool() : void {
+        let account = this.list.get(ManagementAccountsCollection.POS_MEMPOOL);
+        if(account != null){
+            account.reset();
+        }
+    }
+
+    public getFinalizedManagementAccount() : ManagementAccount {
+        return this.getManagementAccount(ManagementAccountsCollection.POS_FINALIZED);
+    }
+
+    public getUnFinalizedManagementAccount() : ManagementAccount {
+        return this.getManagementAccount(ManagementAccountsCollection.POS_UNFINALIZED);
+    }
+
+    public getMempoolManagementAccount() : ManagementAccount {
+        return this.getManagementAccount(ManagementAccountsCollection.POS_MEMPOOL);
+    }
+
+    public getManagementAccount(pos : number) : ManagementAccount {
+        let ac =  this.list.get(pos);
+        if(ac != null){
+            return ac;
+        }
+        throw new NullPointerException("ManagementAccountsCollection.getManagementAccount()");
+    }
+
+    public getTransactionStoreStatus(trxId : TransactionId) : number {
+        let storeType = ManagedUtxoCacheRecord.NONE;
+
+        let memPool = this.getMempoolManagementAccount();
+        let unFinalized = this.getUnFinalizedManagementAccount();
+        let finalized = this.getFinalizedManagementAccount();
+
+        // mempool
+        if(memPool.hasTransaction(trxId) && !unFinalized.hasTransaction(trxId)) {
+            storeType = ManagedUtxoCacheRecord.MEMPOOL;
+        }
+
+        // unfinalized
+        if(storeType == ManagedUtxoCacheRecord.NONE && unFinalized.hasTransaction(trxId)){
+            storeType = ManagedUtxoCacheRecord.UNFINALIZED;
+        }
+
+        // finalized
+        if(storeType == ManagedUtxoCacheRecord.NONE && finalized.hasTransaction(trxId)){
+            storeType = ManagedUtxoCacheRecord.FINALIZED;
+        }
+
+        return storeType;
+    }
 }
